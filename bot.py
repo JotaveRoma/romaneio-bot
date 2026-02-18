65a07decc5bc31eb7056b4e508404705c684a8b0
João Vitor Roma <jvitorroma@gmail.com>
2026-02-18 11:40:07 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,28 @@
 import os
-from flask import Flask, request
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from flask import Flask, requesthttps://github.com/JotaveRoma/romaneio-bot/blob/main/bot.py
+from telegram import Bot, Update
+from telegram.ext import Dispatcher, CommandHandler
 
 TOKEN = os.environ.get("BOT_TOKEN")
 
 app = Flask(__name__)
-telegram_app = ApplicationBuilder().token(TOKEN).build()
+bot = Bot(token=TOKEN)
+dispatcher = Dispatcher(bot, None, use_context=True)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot online 🚀")
+def start(update, context):
+    update.message.reply_text("Bot online 🚀")
 
-telegram_app.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("start", start))
 
 @app.route("/webhook", methods=["POST"])
-async def webhook():
-    data = request.get_json(force=True)
-    update = Update.de_json(data, telegram_app.bot)
-    await telegram_app.process_update(update)
+def webhook():
+    update = Update.de_json(request.get_json(force=True), bot)
+    dispatcher.process_update(update)
     return "ok"
 
 @app.route("/")
-def home():
+def index():
     return "Bot rodando"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    telegram_app.initialize()
-    telegram_app.start()
-    app.run(host="0.0.0.0", port=port)
-
+    app.run(port=10000)
