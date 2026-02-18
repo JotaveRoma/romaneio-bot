da41062cb208a21b4904f144f563b08f75886113
João Vitor Roma <jvitorroma@gmail.com>
2026-02-18 14:07:31 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,32 +6,10 @@ app = Flask(__name__)
 
 TOKEN = os.environ.get("TOKEN")
 
-def enviar_mensagem(chat_id, texto):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    payload = {
-        "chat_id": chat_id,
-        "text": texto
-    }
-    requests.post(url, json=payload)
-
 @app.route("/")
 def home():
     return "Bot rodando 🚀", 200
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    data = request.get_json()
-
-    if "message" in data:
-        chat_id = data["message"]["chat"]["id"]
-        texto = data["message"].get("text")
-
-        if texto == "/start":
-            enviar_mensagem(chat_id, "Bot online 🚀")
-
     return "ok", 200
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
